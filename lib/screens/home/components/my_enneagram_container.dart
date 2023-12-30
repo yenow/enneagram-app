a2cbf69dfc093316b05691f3f585065104dfe332
yun sinyoung <phantom_ysy@naver.com>
2023-12-31 00:24:36 +0900

    [feature] '자세히' 버튼 추가

--- lib/screens/home/components/my_enneagram_container.dart
+++ lib/screens/home/components/my_enneagram_container.dart
@@ -27,17 +27,17 @@ class MyEnneagramContainer extends StatelessWidget {
   }
 
   Container noEnneagramContainer() => Container(
-    padding: EdgeInsets.symmetric(vertical: 5.h),
-    child: AutoSizeText(
-      '에니어그램 9가지 유형',
-      style: Get.textTheme.titleMedium,
-      maxLines: 1,
-    ),
-  );
+        padding: EdgeInsets.symmetric(vertical: 5.h),
+        child: AutoSizeText(
+          '에니어그램 9가지 유형',
+          style: Get.textTheme.titleMedium,
+          maxLines: 1,
+        ),
+      );
 
   Container myEnneagramContainer() {
     return Container(
-      height: 140.h,
+      height: 170.h,
       decoration: BoxDecoration(
         border: Border.all(color: Colors.grey, width: 0.5),
         borderRadius: const BorderRadius.all(
@@ -47,22 +47,25 @@ class MyEnneagramContainer extends StatelessWidget {
       child: Column(
         mainAxisSize: MainAxisSize.max,
         children: [
-          // 타이틀
-          Container(
-            padding: EdgeInsets.only(
-              top: 10.h,
-            ),
-            child: AutoSizeText(
-              '나의 에니어그램',
-              style: Get.textTheme.titleMedium,
-              maxLines: 1,
-            ),
-          ),
-          // 나의 에니어그램
+          titleContainer(),
+          buildDateDropDown(),
           Expanded(
             child: EnneagramContainer(),
           ),
         ],
+      ),
+    );
+  }
+
+  Container titleContainer() {
+    return Container(
+      padding: EdgeInsets.only(
+        top: 10.h,
+      ),
+      child: AutoSizeText(
+        '나의 에니어그램',
+        style: Get.textTheme.titleMedium,
+        maxLines: 1,
       ),
     );
   }
@@ -73,16 +76,27 @@ class MyEnneagramContainer extends StatelessWidget {
       children: [
         /// 나의 에니어그램 이미지
         Padding(
-          padding: const EdgeInsets.only(left: 15, right: 15, bottom: 10),
+          padding: EdgeInsets.only(left: 15.w, right: 15.w, bottom: 20.h),
           child: Image.asset(
-            enneagramMap[enneagramType]!.imageUrl!,
             height: 50.h,
-            width: 50.h,
+            width: 50.w,
             alignment: Alignment.topCenter,
+            enneagramMap[enneagramType]!.imageUrl!,
           ),
         ),
         Expanded(
           child: buildEnneagramHorizontalDescription(),
+        ),
+        Container(
+          width: 70.w,
+          padding: EdgeInsets.only(left: 5.w, right: 5.w, top: 10.h, bottom: 20.h),
+          child: ElevatedButton(
+            child: const Text('자세히'),
+            style: ButtonStyle(
+              padding: MaterialStateProperty.all(EdgeInsets.zero),
+            ),
+            onPressed: () {},
+          ),
         ),
       ],
     );
@@ -94,7 +108,7 @@ class MyEnneagramContainer extends StatelessWidget {
       mainAxisAlignment: MainAxisAlignment.spaceBetween,
       children: [
         Container(
-          padding: EdgeInsets.only(top: 15.h, bottom: 5.h),
+          padding: EdgeInsets.only(top: 5.h, bottom: 5.h),
           alignment: Alignment.centerLeft,
           child: Text(
             enneagramMap[enneagramType]!.getName(),
@@ -103,11 +117,14 @@ class MyEnneagramContainer extends StatelessWidget {
           ),
         ),
         Expanded(
-          child: AutoSizeText(
-            enneagramMap[enneagramType]!.secondDescription,
-            maxLines: 2,
+          child: Container(
+            alignment: Alignment.topLeft,
+            child: AutoSizeText(
+              maxLines: 2,
+              enneagramMap[enneagramType]!.secondDescription,
+            ),
           ),
-        )
+        ),
       ],
     );
   }
@@ -118,7 +135,6 @@ class MyEnneagramContainer extends StatelessWidget {
       child: DateDropDown(
         list: AppController.to.user.value.enneagramResults.map((e) => dateFormatter.format(e.createdAt)).toList(),
         callback: (String date) {
-          logger.d('click date = $date');
           EnneagramResult enneagramResult =
               AppController.to.user.value.enneagramResults.where((element) => date == dateFormatter.format(element.createdAt)).first;
 
